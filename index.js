24b77a0653e9a96826c09ceecca43795e6270b91
SiddiqueAhmed <153608121+Siddique-Ahmed@users.noreply.github.com>
2024-12-07 20:11:26 +0500

    set port

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.delete("/delete/:id", async (req, res) => {
   }
 });
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT,()=>{
   console.log(`Server is running on port ${PORT}`);
